49743445419934bfc1af8a9ef82438c9a03917d4
Peter Williams <pedroeldiablo@yahoo.com>
2019-12-03 11:12:53 +0000

    starting a new version of ecommerce store

---
+++ client/build/precache-manifest.333d814098a7031880d5a983fd319925.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08b0110a87e268329fa0995d38967d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd209ee183bac4551d62",
+    "url": "/static/css/10.1e0915b5.chunk.css"
+  },
+  {
+    "revision": "42cec0fe267dbfd419de",
+    "url": "/static/css/4.18585a14.chunk.css"
+  },
+  {
+    "revision": "f3bd8fa62dbdd9822c2f",
+    "url": "/static/css/5.8d6e1148.chunk.css"
+  },
+  {
+    "revision": "0abe27ccdd5a27c7dd26",
+    "url": "/static/css/6.31b2c0b8.chunk.css"
+  },
+  {
+    "revision": "378ad74c251f56c7fba7",
+    "url": "/static/css/7.badc8bc3.chunk.css"
+  },
+  {
+    "revision": "3394ed712e3bec5f4f8a",
+    "url": "/static/css/8.db5151bf.chunk.css"
+  },
+  {
+    "revision": "7be4297cc86680cc48c9",
+    "url": "/static/css/9.a7ecebd1.chunk.css"
+  },
+  {
+    "revision": "41f86e73a8b05680119a",
+    "url": "/static/css/main.8df93738.chunk.css"
+  },
+  {
+    "revision": "fd209ee183bac4551d62",
+    "url": "/static/js/10.f67bac82.chunk.js"
+  },
+  {
+    "revision": "0592cd6bb1f6bcfdc223",
+    "url": "/static/js/11.723df0d9.chunk.js"
+  },
+  {
+    "revision": "cf9dcb6a0c2e3a473712",
+    "url": "/static/js/2.4778e94b.chunk.js"
+  },
+  {
+    "revision": "dfd00e3d5aea80e8bb4f",
+    "url": "/static/js/3.40793ef7.chunk.js"
+  },
+  {
+    "revision": "42cec0fe267dbfd419de",
+    "url": "/static/js/4.7d2da5d9.chunk.js"
+  },
+  {
+    "revision": "f3bd8fa62dbdd9822c2f",
+    "url": "/static/js/5.125f4939.chunk.js"
+  },
+  {
+    "revision": "0abe27ccdd5a27c7dd26",
+    "url": "/static/js/6.0b507608.chunk.js"
+  },
+  {
+    "revision": "378ad74c251f56c7fba7",
+    "url": "/static/js/7.e34cea82.chunk.js"
+  },
+  {
+    "revision": "3394ed712e3bec5f4f8a",
+    "url": "/static/js/8.92ac4b4f.chunk.js"
+  },
+  {
+    "revision": "7be4297cc86680cc48c9",
+    "url": "/static/js/9.f8f29e58.chunk.js"
+  },
+  {
+    "revision": "41f86e73a8b05680119a",
+    "url": "/static/js/main.06dd296d.chunk.js"
+  },
+  {
+    "revision": "c586e3c4dbd37d63844c",
+    "url": "/static/js/runtime~main.4a4e363b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- client/src/pages/contact/contact.component.jsx
+++ client/src/pages/contact/contact.component.jsx
@@ -14,7 +14,7 @@ const ContactPage = () => {
     <div className="contact-page">
         <h2 className='title'>Contact</h2> 
         <SimpleSlider />  
-        <p>Taking the ecommerce project from course <a href="https://www.udemy.com/course/complete-react-developer-zero-to-mastery/">Complete React Developer Zero to Mastery/</a> as a starting point this project explores some of the structured approaches to buildiing a complex app with react.</p>
+        <p>React Ecommerce Store built with Create React App, Styled Components, Firebase, Stripe and Heroku</p>
         <p>For more projects to get in touch visit <a href="http://www.peternicholaswilliams.com/">www.peternicholaswilliams.com/</a></p>
         <p>Or see <a href="https://github.com/pedroeldiablo/react-ecommerce">Github</a> to see how it's coded.</p>
     </div>
